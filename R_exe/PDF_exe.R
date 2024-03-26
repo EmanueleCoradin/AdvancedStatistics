c0df2fb4ed864efc07651c90fd93a43463ec467e
Emanuele Coradin <emanuele.coradin@studenti.unipd.it>
2024-03-26 19:45:40 +0100

    ready_markdown_exe

---
+++ R_exe/PDF_exe.R
@@ -0,0 +1,34 @@
+#------------- PASCAL DISTRIBUTION ------------- 
+#              (negative binomial)
+
+#Ann and Maggie are playing cards until one of them wins 5 games
+#suppose all games are independent and the probability that Ann wins is 58%
+# what is the probability that they complete in 7 games
+#if the series ends in 7 games, what is the probability that Ann wins ?
+
+#winning probability
+p_win <- 0.58
+
+#------------- BINOMIAL vs POISSON ------------- 
+
+#a company produces screws
+#the probability of a screw to be defective is p = 0.015
+#a box with n = 100 screws is packaged.
+#Compute:
+# the probability that all screws are non defective
+# the defective screws distribution comparing the Binomial and Poisson distributions
+# how many extra screws should the box contain in order to have n = 100 non
+# defective screws with probability greater than 80%
+
+#1)
+p_defective <- 0.015
+n <- 100
+
+#------------- POISSON PROCESS ------------- 
+rate <- 5/(24*60) #birth rate per hour
+#1) Probability that at least 2 babies are born in 6 hours
+
+P <- 
+
+#------------- EXPONENTIAL DISTRIBUTION ------------- 
+# ΔT of events from a Poisson distribuition follows an exponential distribution
